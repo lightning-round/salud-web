fc9bfbe904e007b85d581b7d8c5cbaed5326967d
navicor <miltonivanterreno@gmail.com>
2015-10-18 17:45:14 -0300

    Se crearon los controladores para iniciar y finalizar la conexión con dropbox, y se creó el dropdown para acceder a los datos del perfil

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -34,7 +34,7 @@ angular
               }
       }])
   */
-  .config(function ($routeSegmentProvider , $routeProvider) {
+  .config(function ($routeSegmentProvider , $routeProvider,$locationProvider) {
     $routeSegmentProvider.
       when('/about', 'about').
       when('/home','home').
@@ -51,6 +51,8 @@ angular
       when('/analysis/new','analysis-new').
       when('/home/analyses','home.analyses').
       when('/home/analyses/:id','home.analyses-show').
+      when('/dropbox-auth-start','dropbox-st').
+      when('/dropbox-auth-finish','dropbox-fn').
       segment('login',{
         templateUrl: 'views/login.html',
         controller: 'LoginCtrl'
@@ -117,12 +119,23 @@ angular
       segment('analysis-new',{
         templateUrl: 'views/analysis-new.html',
         controller: 'AnalysisNewCtrl'
+      }).
+      segment('dropbox-st',{
+        templateUrl: 'views/dropbox-auth-start.html',
+        controller: 'DropboxAuthStartCtrl'
+      }).
+      segment('dropbox-fn',{
+        templateUrl: 'views/dropbox-auth-finish.html',
+        controller: 'DropboxAuthFinishCtrl'
       });
 
      $routeProvider.otherwise({redirectTo: '/profileMeasurements'});
 })
-.config(function($modalProvider) {
-  angular.extend($modalProvider.defaults, {
-    html: true
-  });
+  .config(function($modalProvider,$dropdownProvider) {
+    angular.extend($modalProvider.defaults, {
+      html: true
+    });
+    angular.extend($dropdownProvider.defaults, {
+      html: true
+    });
 });
